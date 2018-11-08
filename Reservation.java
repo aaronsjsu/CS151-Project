0ce9b82f537ed5acc99464d64cd2e005ab915777
Aaron Smith <33443869+aaronsjsu@users.noreply.github.com>
2018-11-07 22:17:00 -0800

    initial commit

---
+++ Reservation.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+
+/**
+ * Models a user reservation in the hotel reservation system. Each reservation record 
+ * knows the User that made the reservation, which room the reservation is for, and 
+ * the time period for the reservation.
+ */
+public class Reservation {
+	private User user;
+	private Room room;
+	private LocalDate startTime;
+	private LocalDate endTime;
+	
+	public Reservation() {
+		
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public Room getRoom() {
+		return room;
+	}
+	
+}
---
+++ Room.java
@@ -0,0 +1,22 @@
+
+/**
+ * Models a hotel room for the hotel reservation system. Each room 
+ * keeps track of its number and rate. 
+ */
+public class Room {
+	private int number;
+	private int rate;
+	
+	public Room(int number, int rate) {
+		this.number = number;
+		this.rate = rate;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	
+	public int getRate() {
+		return rate;
+	}
+}

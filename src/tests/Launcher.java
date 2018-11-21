4a3a677e1dcdbe73d55ff0e705f3956895df4eeb
Aaron Smith <aaron.smith@sjsu.edu>
2018-11-21 14:44:13 -0800

    added program launcher

---
+++ src/tests/Launcher.java
@@ -0,0 +1,15 @@
+package tests;
+
+import gui.InitialScreen;
+
+/**
+ * Launches the initial screen.
+ */
+public class Launcher {
+	public static void main(String args[]) {
+		int screenSize = 750;
+		
+		InitialScreen screen = new InitialScreen(screenSize);
+		
+	}
+}

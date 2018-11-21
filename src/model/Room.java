0509a7ac053f810a54270bb66bc981b14d388364
Kevin Tyrrell <kev070892@gmail.com>
2018-11-21 09:09:12 -0800

    Formalized core model classes. * Reservations of users are now properly encapsulated. * Documentation added for room and user. * Added a validation interface within the user class. * Password are no longer saved as plain-text.

--- src/model/Room.java
+++ src/model/Room.java
@@ -4,20 +4,39 @@ package model;
  * Models a hotel room for the hotel reservation system. Each room 
  * keeps track of its number and rate. 
  */
-public class Room {
-	private int number;
-	private int rate;
-	
-	public Room(int number, int rate) {
+public final class Room
+{
+	/* Number of the room. */
+	private final int number;
+	/* Rate at which is charged for the room. */
+	private final int rate;
+
+	/**
+	 * Constructs a room inside the hotel.
+	 * @param number Room number of the hotel.
+	 * @param rate Rate to charge for the room.
+	 */
+	public Room(final int number, final int rate)
+	{
+		if (number < 0) throw new IllegalArgumentException("Room number must be positive!");
+		if (rate < 0) throw new IllegalArgumentException("Going rate for the room must be positive!");
 		this.number = number;
 		this.rate = rate;
 	}
-	
-	public int getNumber() {
+
+	/**
+	 * @return Number of the room.
+	 */
+	public int getNumber()
+	{
 		return number;
 	}
-	
-	public int getRate() {
+
+	/**
+	 * @return Rate which is charged for the room.
+	 */
+	public int getRate() 
+	{
 		return rate;
 	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,42 +1,79 @@
 package model;
 
-import java.util.List;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Models a user account in the hotel reservation system. Each account keeps track of a user id, 
  * password, username, and all reservations that the user has made (excluding canceled ones). 
  */
-public class User {
-	private String userID;
-	private String password;
-	private String username;
-	private List<Reservation> reservations;
-	
-	public User(String userID, String password, String username) {
-		this.userID = userID;
-		this.password = password;
-		this.username = username;
-		reservations = new ArrayList<Reservation>();
+public final class User implements Serializable
+{
+    /* Core user credentials. */
+	private final String id, username;
+	/* Hashed password used to authenticate login. */
+	private final byte[] password;
+	/* Reservations corresponding to the user. */
+	private final List<Reservation> reservations = new ArrayList<>();
+
+    /**
+     * Constructs a user with the specified credentials.
+     * Plaintext passwords are not stored with the user.
+     * @param id ID of the user.
+     * @param username Username used for login purposes.
+     * @param password Password used for login purposes.
+     */
+	public User(final String id, final String username, final String password)
+    {
+        this.id = Objects.requireNonNull(id);
+        this.username = Objects.requireNonNull(username);
+        this.password = Security.hash(Objects.requireNonNull(password));
 	}
-	
-	public void addReservation(Reservation r) {
-		reservations.add(r);
+
+    /**
+     * Adds a reservation to the user's account.
+     * @param reservation Reservation to be added.
+     */
+	public void addReservation(final Reservation reservation)
+    {
+        reservations.add(Objects.requireNonNull(reservation));
 	}
-	
-	public String getUserID() {
-		return userID;
+
+    /**
+     * Indicates if a specified password is valid for this user.
+     * @param password Password to be validated.
+     * @return true if the password matches the user's credentials.
+     */
+	public boolean validatePassword(final String password)
+    {
+        return Arrays.equals(Security.hash(Objects.requireNonNull(password)), this.password);
+    }
+
+    /**
+     * @return ID of the user.
+     */
+	public String getID()
+    {
+		return  id;
 	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public String getUsername() {
+
+    /**
+     * @return Username of the user.
+     */
+	public String getUsername()
+    {
 		return username;
 	}
-	
-	public List<Reservation> getReservations() {
-		return reservations;
+
+    /**
+     * @return Read-only view of the reservations the user has made.
+     */
+	public List<Reservation> getReservations()
+    {
+		return Collections.unmodifiableList(reservations);
 	}
 }

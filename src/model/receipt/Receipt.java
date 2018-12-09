412e92848acf7f56e244fd38d885525c9bdcb9f6
Kevin Tyrrell <kev070892@gmail.com>
2018-12-08 23:07:28 -0800

    Restructured formatter.

--- src/model/receipt/Receipt.java
+++ src/model/receipt/Receipt.java
@@ -5,28 +5,34 @@ import model.Reservation;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Receipt {
-  private List<Reservation> reservedRooms;
-  private int  totalAmount;
-  private String userName;
+/**
+ * Models a receipt which details rooms reserved by the user.
+ */
+public class Receipt
+{
+    private List<Reservation> reservedRooms;
+    private int totalAmount;
+    private String userName;
+    
+    Receipt()
+    {
+        reservedRooms = new ArrayList<>();
+        totalAmount = 0;
+        userName = "";
+    }
 
-  Receipt() {
-    reservedRooms = new ArrayList<>();
-    totalAmount = 0;
-    userName = "";
-  }
+    public void setReservedRooms(List<Reservation> reservedRooms)
+    {
+        this.reservedRooms = reservedRooms;
+    }
 
-  public void setReservedRooms(List<Reservation> reservedRooms) {
-    this.reservedRooms = reservedRooms;
-  }
+    public void setTotalAmount(int totalAmount) {
+        this.totalAmount = totalAmount;
+    }
 
-  public void setTotalAmount(int totalAmount) {
-    this.totalAmount = totalAmount;
-  }
-
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
 
 }
